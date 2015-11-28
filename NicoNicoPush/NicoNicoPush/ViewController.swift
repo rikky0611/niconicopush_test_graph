afab95064972292ce940a7cbae19449a147f59ac
Aguri <aguri1120@gmail.com>
2015-11-28 12:56:22 +0900

    プロジェクト作った！

---
+++ NicoNicoPush/NicoNicoPush/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NicoNicoPush
+//
+//  Created by 奥薗阿具利 on 2015/11/28.
+//  Copyright © 2015年 奥薗阿具利. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
